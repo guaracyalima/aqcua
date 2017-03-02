6c6b3f906a91ce5a5531c8a9af6d634ac7ef437f
guaracyalima <guaracyaraujolima@gmail.com>
2017-03-02 17:17:37 -0300

    updates

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -159,15 +159,6 @@
     <!-- Application Start SCRIPTS -->
     <script src="app/site/controllers/main.ctrl.js"></script>
     <script src="app/site/controllers/cart.ctrl.js"></script>
-    {{--<div id="fb-root"></div>--}}
-    {{--<script>(function(d, s, id) {--}}
-            {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
-            {{--if (d.getElementById(id)) return;--}}
-            {{--js = d.createElement(s); js.id = id;--}}
-            {{--js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";--}}
-            {{--fjs.parentNode.insertBefore(js, fjs);--}}
-        {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
-    {{----}}
 
 
 </body>
